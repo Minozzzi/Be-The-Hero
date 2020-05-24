d5732a99f06a77c94f76df9bc8f2c3f27b6253d1
Guilherme Minozzi <minozzi60@gmail.com>
2020-05-24 11:37:52 -0300

    Aplicação arrumada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+
 import './global.css';
 
 import Routes from './routes';
 
 function App() {
   return (
-      <Routes />
-    
+    <Routes />
   );
 }
 
